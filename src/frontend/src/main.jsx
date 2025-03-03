a6359d7ad5e47a03939b86ac6b5b481719e6113c
Joe Parrish <jparrish@thevariable.com>
2025-03-02 20:22:44 -0500

    Simplified React app for debugging Firebase issues

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ChakraProvider, Box, Text, extendTheme } from '@chakra-ui/react'
 
+// Super simple app for debugging
+const App = () => {
+  return (
+    <Box p={5} textAlign="center">
+      <Text fontSize="xl">Hello World - Basic React App</Text>
+      <Text mt={4}>Environment: {window.location.href}</Text>
+    </Box>
+  )
+}
+
+// Create theme
+const theme = extendTheme({})
+
+// Render the app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
